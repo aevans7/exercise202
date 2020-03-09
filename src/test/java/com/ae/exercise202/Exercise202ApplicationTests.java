4db7223589ad7bdeaaf5995df7de5192f6d40a7a
GBTC440014UR <aevans7>
2020-03-09 18:06:24 -0400

    Initial commit -- exercise 2.02 complete

---
+++ src/test/java/com/ae/exercise202/Exercise202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.exercise202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
